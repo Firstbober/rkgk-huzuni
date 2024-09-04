9a0a259a22300e59b2713ddf167133cf29256ab2
Firstbober <firstbober@tutanota.com>
2024-09-04 20:35:57 +0200

    Add importing native rkgk modules.

--- src/rkgk-huzuni/index.ts
+++ src/rkgk-huzuni/index.ts
@@ -85,12 +85,104 @@ const rightPanel = {
   },
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function body(pMap) {
+  // do something with module pMap here
+}
+
 globalThis.huzuni = {
   unstable: {
     topPanel,
     rightPanel,
   },
+  rkgk: {
+    handleRkGkImports: async (
+      body,
+      name: string,
+      path: string,
+      imported: boolean,
+    ) => {
+      if (imported) {
+        globalThis.huzuni.rkgk[name] = body;
+        return;
+      }
+      await import(`${path}`).then(
+        async (i) =>
+          await globalThis.huzuni.rkgk.handleRkGkImports(i, name, path, true),
+      );
+    },
+  },
 };
+
+async function huzuniStartupImportsAndHijack() {
+  await globalThis.huzuni.rkgk.handleRkGkImports(
+    null,
+    'wall',
+    './static/wall.js',
+    false,
+  );
+  await globalThis.huzuni.rkgk.handleRkGkImports(
+    null,
+    'session',
+    './static/session.js',
+    false,
+  );
+  await globalThis.huzuni.rkgk.handleRkGkImports(
+    null,
+    'framework',
+    './static/framework.js',
+    false,
+  );
+  await globalThis.huzuni.rkgk.handleRkGkImports(
+    null,
+    'reticle_renderer',
+    './static/reticle-renderer.js',
+    false,
+  );
+
+  // // Disable index.js
+  // for(const script of document.scripts) {
+  //   if(script.src.includes('static/index.js')) {
+  //     script.addEventListener('beforescriptexecute', async (event) => {
+  //       console.warn("[huzuni] disabling index.js")
+  //       event.preventDefault()
+  //     })
+  //   }
+  // }
+
+  // // Hijack and modify index.js
+  // {
+  //   console.warn('[huzuni] hijacking index.js');
+  //   const code = await (await fetch('static/index.js')).text();
+
+  //   let gatekeeper = true
+  //   let newCode = '';
+  //   for(const line of code.split('\n')) {
+  //     if(line.startsWith("const updateInterval = 1000 / 60;")) {
+  //       gatekeeper = false;
+  //     }
+
+  //     if(gatekeeper) continue
+  //     newCode += line;
+  //   }
+
+  //   globalThis.huzuni.rkgk.index = {};
+
+  //   eval(`
+  //     const Wall = huzuni.rkgk.wall.Wall;
+
+  //     const getLoginSecret = huzuni.rkgk.session.getLoginSecret;
+  //     const getUserId = huzuni.rkgk.session.getUserId;
+  //     const isUserLoggedIn = huzuni.rkgk.session.isUserLoggedIn;
+  //     const newSession = huzuni.rkgk.session.newSession;
+  //     const registerUser = huzuni.rkgk.session.registerUser;
+  //     const waitForLogin = huzuni.rkgk.session.waitForLogin;
+
+  //     const debounce = huzuni.rkgk.framework.debounce;
+  //     const ReticleCursor = huzuni.rkgk.reticle_renderer.ReticleCursor;
+  //   `+ newCode);
+  // }
+}
 
 function huzuniStartupPlugins() {
   // Huzuni logo
@@ -114,4 +206,5 @@ function huzuniStartupPlugins() {
   // Script manager
 }
 
+huzuniStartupImportsAndHijack();
 huzuniStartupPlugins();
